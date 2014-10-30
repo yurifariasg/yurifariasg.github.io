034d4c521d12f95e00c110948f86a917b5e73a18
Yuri Farias Gomes <yurifariasg@gmail.com>
2014-10-29 21:56:20 -0700

    Added Jawbone internship. Updated skills. Added resume. Added OpenGL presentation.

--- static/js/page.js
+++ static/js/page.js
@@ -28,16 +28,17 @@ var onLoadBody = function() {
     Page.addSkill("working-skills", "Python");
     Page.addSkill("working-skills", "Tomcat");
     Page.addSkill("working-skills", "OpenGL ES 1.1");
+    Page.addSkill("working-skills", "MySQL");
+    Page.addSkill("working-skills", "HTTP");
 
 
     Page.addSkill("basic-skills", "iOS");
+    Page.addSkill("basic-skills", "C++");
     Page.addSkill("basic-skills", "Qt");
     Page.addSkill("basic-skills", "Grails");
     Page.addSkill("basic-skills", "Flask");
-    Page.addSkill("basic-skills", "C++");
     Page.addSkill("basic-skills", "C");
     Page.addSkill("basic-skills", "Lua");
-    Page.addSkill("basic-skills", "MySQL");
     Page.addSkill("basic-skills", "OpenGL");
     Page.addSkill("basic-skills", "Grails");
     Page.addSkill("basic-skills", "Photoshop CS3");
